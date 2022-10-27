f12ee2e92218575a16e3b651d6cc7fdaedddcad2
Igor Pokrovskiy <iokcu@yandex.ru>
2022-10-28 02:43:28 +0400

    Refactoring after review

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,22 @@
-//Форма добавления карточки
-export const addPhotoButton = document.querySelector('.profile__add-button');
-export const popupAddPhoto = document.getElementById('popup-add-photo');
-export const popupAddPhotoClose = popupAddPhoto.querySelector('.popup__close');
+import { closePopupEscape } from "./modal";
+const popups = document.querySelectorAll('.popup')
+//Функции открытия и закрытия попапа
+      popups.forEach((popup) => {
+          popup.addEventListener('mousedown', (evt) => {
+              if (evt.target.classList.contains('popup_opened')) {
+                  closePopup(popup)
+              }
+              if (evt.target.classList.contains('popup__close')) {
+                closePopup(popup)
+              }
+          })
+      })
+export function openPopup(popup) {
+    popup.classList.add("popup_opened");
+    document.addEventListener('keydown', closePopupEscape);
+  }
+  
+export function closePopup(popup) {
+    popup.classList.remove("popup_opened");
+    document.removeEventListener('keydown', closePopupEscape);
+  }
