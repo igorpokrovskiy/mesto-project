eef7c2cbe682ae9deefad6a319059fa7b7881915
Igor Pokrovskiy <iokcu@yandex.ru>
2022-10-28 20:27:39 +0400

    Validation settings config

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,16 +1,5 @@
 import { closePopupEscape } from "./modal";
-import { popups } from "./constants.js"
-//Функции открытия и закрытия попапа
-      popups.forEach((popup) => {
-          popup.addEventListener('mousedown', (evt) => {
-              if (evt.target.classList.contains('popup_opened')) {
-                  closePopup(popup)
-              }
-              if (evt.target.classList.contains('popup__close')) {
-                closePopup(popup)
-              }
-          })
-      })
+
 export function openPopup(popup) {
     popup.classList.add("popup_opened");
     document.addEventListener('keydown', closePopupEscape);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { renderInitialCards, handleAddPhotoSubmit } from "./components/card.js";
 import { openPopup } from './components/utils.js';
 import {profileEditButton,popupEditProfile, profileForm, handleProfileFormSubmit, profileName, profileAbout, nameInput, jobInput} from "./components/modal.js"
 import { enableValidation, settings } from './components/validate.js';
-import { addPhotoButton, popupAddPhoto } from './components/constants.js'
+import { addPhotoButton, popupAddPhoto, popups } from './components/constants.js'
 
 profileEditButton.addEventListener('click', () => {
     openPopup(popupEditProfile);
@@ -16,6 +16,18 @@ addPhotoButton.addEventListener('click', () => {
   });
 popupAddPhoto.addEventListener('submit', handleAddPhotoSubmit); 
 profileForm.addEventListener('submit', handleProfileFormSubmit); 
+
+//Функции открытия и закрытия попапа
+popups.forEach((popup) => {
+  popup.addEventListener('mousedown', (evt) => {
+      if (evt.target.classList.contains('popup_opened')) {
+          closePopup(popup)
+      }
+      if (evt.target.classList.contains('popup__close')) {
+        closePopup(popup)
+      }
+  })
+})
 
 renderInitialCards();
 enableValidation(settings);
